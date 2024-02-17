f9ec7672610dcb2da10e5e158fd4d6db38a4068c
Zak <shearmanzak@gmail.com>
2024-02-17 14:57:08 +0000

    Add writer to context

--- pkg/oapi-rt/context.go
+++ pkg/oapi-rt/context.go
@@ -11,19 +11,25 @@ import (
 type contextKey string
 
 const (
+	writerKey  contextKey = "writer"
 	requestKey contextKey = "request"
 	acceptKey  contextKey = "accept"
 )
 
-func NewContext(ctx context.Context, r *http.Request) context.Context {
-	return context.WithValue(
-		context.WithValue(ctx, acceptKey, r.Header.Get("Accept")),
-		requestKey, r,
-	)
+func NewContext(ctx context.Context, w http.ResponseWriter, r *http.Request) context.Context {
+	ctx = context.WithValue(ctx, writerKey, w)
+	ctx = context.WithValue(ctx, requestKey, r)
+	return context.WithValue(ctx, acceptKey, r.Header.Get("Accept"))
 }
 
 // RequestFromContext returns the request from the context.
 // Bad bad very bad do not use unless necessary
 func RequestFromContext(ctx context.Context) *http.Request {
 	return ctx.Value(requestKey).(*http.Request)
+}
+
+// WriterFromContext returns the writer from the context.
+// Bad bad very bad by zak
+func WriterFromContext(ctx context.Context) http.ResponseWriter {
+	return ctx.Value(writerKey).(http.ResponseWriter)
 }
